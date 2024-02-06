7c28a0236e66882f22da253913586ed9ee0aa657
mrking <869903116@qq.com>
2024-02-06 21:51:06 +0800

    test for Qwen train data

--- demo/origin_data/xiaomi_train_data/test_table.py
+++ demo/origin_data/xiaomi_train_data/test_table.py
@@ -3,7 +3,7 @@ import json
 with open("/home/susu/下载/c_question/prep_c_train_data/prep_c_train_data/data/tables.json", "r", encoding="utf-8") as f:
     tables_data = json.load(f)
 
-with open("test.json", "w", encoding="utf-8") as f:
+with open("test_table.json", "w", encoding="utf-8") as f:
     json.dump(tables_data, f, indent=2, ensure_ascii=False)
     print("Done!")
 
